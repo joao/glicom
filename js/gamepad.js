cb3dd4074e7823bf0b6e0d790acd8b386c0b61ac
Pedro Ângelo <pangelo@void.io>
2022-03-24 13:56:24 +0000

    Make gamepad button mapping more flexible
    We can now assign explicit button ids to specific character codes.

--- js/gamepad.js
+++ js/gamepad.js
@@ -7,9 +7,17 @@
     // set this to true to log button events to the console 
     // (useful to discover button id's) for a new controller
     const LOG_BUTTON_PRESSES = true;
+
+    // gamepad device state
     let pad = {
         device: null,
-        buttons: [false, false, false, false],
+        mapping: [
+            // [button_id, char_code_to_inject, last_button_state] 
+            [8,  'a', false],
+            [9,  'b', false],
+            [10, 'c', false], 
+            [11, 'd', false]
+        ]
     };
     
     window.addEventListener("gamepadconnected", (ev) => {
@@ -44,20 +52,23 @@
                 }
             }
         }
+
+        for (button_mapping of pad.mapping) {
+            let [button_id, to_char, last_state] = button_mapping;
             let new_state = pad_state.buttons[button_id].pressed;
-            if (new_state != pad.buttons[button_id]) {
+            if (new_state != last_state) {
                 if (new_state === false) {
-                    inject_keypress(button_id);
+                    inject_keypress (to_char);
                 }
-                pad.buttons[button_id] = new_state;
+                button_mapping[2] = new_state;
             }
         }
         
         window.requestAnimationFrame(poll_pads);
     }
     
-    const inject_keypress = (button_id) => {
-        let ev = new KeyboardEvent ("keydown", { bubbles: true, cancelable: true, key: KEY_MAP[button_id], char: KEY_MAP[button_id] });
+    const inject_keypress = (char_code) => {
+        let ev = new KeyboardEvent ("keydown", { bubbles: true, cancelable: true, key: char_code, char: char_code });
         document.body.dispatchEvent(ev);
     }
 }
